394e65f17f537114bd7becb23dd5fa53cbc8a7b5
root <root@DESKTOP-922GFR1.localdomain>
2022-11-30 00:00:12 +0100

    Why did you have to make me suffer so much?

---
+++ app.js
@@ -0,0 +1,11 @@
+function validate(){
+    let stringVal = document.getElementById('stringVal').
+    value;
+
+    if (stringVal == 'mop'){
+        alert('Hejka Julia');
+    } else {
+        alert('Oj nie znam takiego loginu :(')
+    }
+}
+
--- phpBack/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>LOSOWANKO</title>
-    </head>
-    <body>
-        <h1>test</h1>
-    </body>
-</html>
